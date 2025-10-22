37296788b29821411fc611ebf4ef8c964ffdbd5d
bianca-isbly <biancaisabely7@gmail.com>
2025-10-22 11:57:26 -0300

    instruções

--- Index.js
+++ Index.js
@@ -1,3 +1,36 @@
-camel case -> nomeAluno
-upper camel case -> NomeAluno
-snake case -> nome_aluno
+// camel case -> nomeAluno
+// upper camel case -> NomeAluno
+// snake case -> nome_aluno
+
+// var curso = "Lions Dev"
+
+//console. log(curso)
+
+//let nome = 'Alice'
+//let mensagem = "Bem-vindo ao mundo JavaScript! ";
+//const meuValorNPodeSerAlterado = 42;
+
+//console.log(mensagem + nome);
+
+// crie uma variavel chamda meu nome e outra chamda sua Idade e imprima os valores na tela
+
+let meuNome = 'Bianca '
+let idade = 23
+
+console.log(meuNome + idade)
+
+
+
+// _+ - * /
+
+const numero1 = 10
+const numero2 = 20
+let soma = numero1 + numero2
+
+console.log(soma)
+
+const numero3 = 5
+const numero4 = 10
+let multip = numero3 * numero4
+
+console.log(multip)
