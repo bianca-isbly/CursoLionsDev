2c7b6f71fd76d81b659c9682e03995a6fb4beeb8
bianca-isbly <biancaisabely7@gmail.com>
2025-10-23 11:25:31 -0300

    variáveis

--- Index.js
+++ Index.js
@@ -14,23 +14,32 @@
 
 // crie uma variavel chamda meu nome e outra chamda sua Idade e imprima os valores na tela
 
-let meuNome = 'Bianca '
-let idade = 23
+//let meuNome = 'Bianca '
+//let idade = 23
+//let signo = ' Câncer'
 
-console.log(meuNome + idade)
-
-
+//console.log(meuNome + idade + signo)
 
 // _+ - * /
 
-const numero1 = 10
-const numero2 = 20
-let soma = numero1 + numero2
+//const numero1 = 10;
+//const numero2 = 20;
+//let soma = numero1 + numero2;
 
-console.log(soma)
+//console.log(soma);
 
-const numero3 = 5
-const numero4 = 10
-let multip = numero3 * numero4
+//const numero3 = 5;
+//const numero4 = 10;
+//let multip = numero3 * numero4;
 
-console.log(multip)
+//console.log(multip);
+
+//let mensagem = 'Hello World - ';
+
+//console.log(mensagem + meuNome + idade + signo);
+
+let nome = 'Bia ';
+let mensagem = 'Developer in Training.';
+let saudacao = nome + mensagem;
+
+console.log(saudacao);
